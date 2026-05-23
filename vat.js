7dd238733bf93630c28cb138fadd597405ba90ef
kamlesh04yadav15-ux <kamlesh04yadav15@gmail.com>
2026-05-23 11:51:14 +0530

    Update vat.js

--- vat.js
+++ vat.js
@@ -1,1 +1,23 @@
+function calculateVAT(){
 
+    let amount = parseFloat(
+        document.getElementById("amount").value
+    );
+
+    if(isNaN(amount)){
+
+        alert("Please Enter Amount");
+
+        return;
+    }
+
+    let vat = amount * 0.05;
+
+    let total = amount + vat;
+
+    document.getElementById("vatAmount").innerHTML =
+        "VAT (5%): AED " + vat.toFixed(2);
+
+    document.getElementById("totalAmount").innerHTML =
+        "Total Amount: AED " + total.toFixed(2);
+}
